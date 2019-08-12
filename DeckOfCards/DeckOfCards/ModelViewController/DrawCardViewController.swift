4a8c903a0e45d8c49f9dcb059df63fef7ce40e69
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-12 11:01:45 -0600

    Finished adding MVC Files

---
+++ DeckOfCards/DeckOfCards/ModelViewController/DrawCardViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DrawCardViewController.swift
+//  DeckOfCards
+//
+//  Created by AlphaDVLPR on 8/12/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+import UIKit
+
+class DrawCardViewController: UIViewController {
+    
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var suitLabel: UILabel!
+    @IBOutlet weak var cardImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func drawButtonTapped(_ sender: Any) {
+        
+    }
+    
+    
+}
